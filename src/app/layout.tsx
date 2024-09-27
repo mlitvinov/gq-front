6e68c23fe8ec3bbe2dfe8180c813d44ea2eb77d2
Philipp <Crisis4433@gmail.com>
2024-09-27 13:53:07 +0300

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { PropsWithChildren } from "react";
+import type { Metadata } from "next";
+
+import { Root } from "@/components/Root/Root";
+
+import "@telegram-apps/telegram-ui/dist/styles.css";
+import "modern-normalize/modern-normalize.css";
+import "./_assets/globals.css";
+
+export const metadata: Metadata = {
+  title: "Your Application Title Goes Here",
+  description: "Your application description goes here",
+};
+
+export default function RootLayout({ children }: PropsWithChildren) {
+  return (
+    <html lang="en">
+      <body>
+        <Root>{children}</Root>
+      </body>
+    </html>
+  );
+}
