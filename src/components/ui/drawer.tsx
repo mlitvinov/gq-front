785c95ce35325638837b736fbc2ffe3c67b602a0
Philipp <Crisis4433@gmail.com>
2024-11-09 23:58:35 +0300

    Изменить позиционирование DrawerContent с фиксированного на абсолютное

--- src/components/ui/drawer.tsx
+++ src/components/ui/drawer.tsx
@@ -44,7 +44,7 @@ const DrawerContent = React.forwardRef<
     <DrawerPrimitive.Content
       ref={ref}
       className={cn(
-        "fixed inset-x-0 bottom-0 z-[9999] mt-24 flex h-auto flex-col rounded-t-[10px] border bg-white",
+        "absolute inset-x-0 bottom-0 z-[9999] mt-24 flex h-auto flex-col rounded-t-[10px] border bg-white",
         className
       )}
       {...props}
