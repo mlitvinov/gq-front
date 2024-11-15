4b0da7902d7d91f3a49d59881d0739d62c2e7917
mlitvinov <46522074+mlitvinov@users.noreply.github.com>
2024-11-15 17:29:03 +0300

    fix

--- src/app/recommendations/page.tsx
+++ src/app/recommendations/page.tsx
@@ -11,7 +11,7 @@ const RecommendationsPage = () => {
       <div className="text-center">
         <img alt="Cat emoji" src={Cat.src} className="w-32 h-32 object-contain mb-6 mx-auto" />
         <h1 className="text-2xl font-black text-black">{t("soon")}</h1>
-        <h1 className="text-2xl text-gradient ml-auto font-semibold text-black">12/12/2024</h1>
+        <h1 className="text-2xl text-gradient ml-auto font-semibold text-black">15/12/2024</h1>
       </div>
     </main>
   );
