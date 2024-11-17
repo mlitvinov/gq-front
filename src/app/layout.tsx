f0f818e66a9c342bc32bcf7da775f1e465cefe1c
Philipp <Crisis4433@gmail.com>
2024-11-17 20:55:23 +0300

    изменить логику получения первого запуска на использование localStorage вместо cookie

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,6 @@ import { I18nProvider } from "@/core/i18n/provider";
 import "@telegram-apps/telegram-ui/dist/styles.css";
 import "normalize.css/normalize.css";
 import "./_assets/globals.css";
-import { getFirstLaunch } from "@/core/firstLaunch";
 
 export const metadata: Metadata = {
   title: "GetQuest",
--- src/core/firstLaunch.ts
+++ src/core/firstLaunch.ts
@@ -2,17 +2,17 @@
 
 // In this example the locale is read from a cookie. You could alternatively
 // also read it from a database, backend service, or any other source.
-const COOKIE_NAME = "IS_FIRST_LAUNCH";
+const STORAGE_NAME = "IS_FIRST_LAUNCH";
 
-const getFirstLaunch = async () => {
-  return localStorage.getItem(COOKIE_NAME) ?? null;
+const getFirstLaunch = () => {
+  return localStorage.getItem(STORAGE_NAME) ?? null;
 };
 
-const setFirstLaunch = async (state?: "YES") => {
+const setFirstLaunch = (state?: "YES") => {
   if (state === "YES") {
-    localStorage.setItem(COOKIE_NAME, state);
+    localStorage.setItem(STORAGE_NAME, state);
   } else {
-    localStorage.removeItem(COOKIE_NAME);
+    localStorage.removeItem(STORAGE_NAME);
   }
 };
 
