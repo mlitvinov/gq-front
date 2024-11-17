d16863b0e8d6ef63590c779ae809a0eb0ab9e5e9
mlitvinov <46522074+mlitvinov@users.noreply.github.com>
2024-11-17 22:32:04 +0300

    Revert "изменить получение локали на использование localStorage вместо cookie"
    This reverts commit 9d350d05f97b7b71236c2455fe0b08ad635d1cd5.

--- src/core/i18n/locale.ts
+++ src/core/i18n/locale.ts
@@ -1,19 +1,21 @@
 //use server is required
-"use client";
+"use server";
+
+import { cookies } from "next/headers";
 
 import { defaultLocale } from "./config";
 import type { Locale } from "./types";
 
 // In this example the locale is read from a cookie. You could alternatively
 // also read it from a database, backend service, or any other source.
-const STORAGE_KEY = "GET_QUEST_LOCALE";
+const COOKIE_NAME = "GET_QUEST_LOCALE";
 
-const getLocale = () => {
-  return localStorage.getItem(STORAGE_KEY) || defaultLocale;
+const getLocale = async () => {
+  return cookies().get(COOKIE_NAME)?.value || defaultLocale;
 };
 
-const setLocale = (locale?: string) => {
-  localStorage.setItem(STORAGE_KEY, (locale as Locale) || defaultLocale);
+const setLocale = async (locale?: string) => {
+  cookies().set(COOKIE_NAME, (locale as Locale) || defaultLocale);
 };
 
 export { getLocale, setLocale };
