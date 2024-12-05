3589f6b96cd397dddf8315391152b516a2ab3f3b
mlitvinov <46522074+mlitvinov@users.noreply.github.com>
2024-12-05 15:49:07 +0300

    add animation

--- src/app/recommendations/page.tsx
+++ src/app/recommendations/page.tsx
@@ -1,9 +1,25 @@
 "use client";
 
 import React, { useEffect, useState, useRef } from "react";
-import { FiMaximize, FiRefreshCw, FiThumbsDown, FiThumbsUp } from "react-icons/fi";
+import { FiMaximize, FiThumbsDown, FiThumbsUp } from "react-icons/fi";
 import { initData, useSignal } from "@telegram-apps/sdk-react";
 import { Link } from "@/components/Link/Link";
+import { motion, AnimatePresence } from "framer-motion";
+
+const variants = {
+  enter: (direction: number) => ({
+    x: direction * 1000,
+    opacity: 0,
+  }),
+  center: {
+    x: 0,
+    opacity: 1,
+  },
+  exit: (direction: number) => ({
+    x: direction * -1000,
+    opacity: 0,
+  }),
+};
 
 const RecommendationsPage = () => {
   const [videoSrc, setVideoSrc] = useState<string | null>(null);
@@ -14,6 +30,9 @@ const RecommendationsPage = () => {
   const [description, setDescription] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(false);
   const videoRef = useRef<HTMLVideoElement>(null);
+
+  const [page, setPage] = useState(0);
+  const [direction, setDirection] = useState(0);
 
   // Получаем initData из контекста
   const initDataRaw = useSignal(initData.raw);
@@ -59,6 +78,7 @@ const RecommendationsPage = () => {
             const blob = await videoResponse.blob();
             const objectUrl = window.URL.createObjectURL(blob);
             setVideoSrc(objectUrl);
+            setPage((prevPage) => prevPage + 1); // Увеличиваем page для анимации
           } else {
             console.error("Ошибка при загрузке видео:", videoResponse.status);
             setVideoSrc(null);
@@ -115,6 +135,7 @@ const RecommendationsPage = () => {
 
   const likeVideo = async () => {
     if (!videoId) return;
+    setDirection(-1); // Анимация влево при лайке
 
     try {
       const headers: HeadersInit = {
@@ -132,7 +153,7 @@ const RecommendationsPage = () => {
 
       if (response.ok) {
         // Загружаем следующее видео
-        fetchVideo();
+        await fetchVideo();
       } else {
         console.error("Ошибка при лайке видео:", response.status);
       }
@@ -143,6 +164,7 @@ const RecommendationsPage = () => {
 
   const dislikeVideo = async () => {
     if (!videoId) return;
+    setDirection(1); // Анимация вправо при дизлайке
 
     try {
       const headers: HeadersInit = {
@@ -160,7 +182,7 @@ const RecommendationsPage = () => {
 
       if (response.ok) {
         // Загружаем следующее видео
-        fetchVideo();
+        await fetchVideo();
       } else {
         console.error("Ошибка при дизлайке видео:", response.status);
       }
@@ -186,36 +208,51 @@ const RecommendationsPage = () => {
   return (
     <div className="min-h-screen">
       <main className="flex flex-col items-center justify-center px-4">
-        <div className="w-72 h-72 rounded-full overflow-hidden mt-8 bg-gray-700 flex items-center justify-center relative">
-          {loading && <p className="text-white">Загрузка...</p>}
-          {videoSrc && !loading && (
-            <>
-              <video
-                ref={videoRef}
-                src={videoSrc}
-                className="w-full h-full object-cover"
-                controls={false}
-                loop
-                playsInline
-                autoPlay
-                onClick={(e) => {
-                  const video = e.currentTarget as HTMLVideoElement;
-                  if (video.paused) {
-                    video.play();
-                  } else {
-                    video.pause();
-                  }
-                }}
-              />
-            </>
-          )}
+        <div className="w-72 h-72 mt-8 relative overflow-hidden">
+          <AnimatePresence initial={false} custom={direction}>
+            <motion.div
+              key={page}
+              className="w-72 rounded-full overflow-hidden  h-72 bg-gray-700 flex items-center justify-center"
+              custom={direction}
+              variants={variants}
+              initial="enter"
+              animate="center"
+              exit="exit"
+              transition={{
+                x: { type: "spring", stiffness: 300, damping: 30 },
+                opacity: { duration: 0.2 },
+              }}
+              style={{ position: "absolute", top: 0, left: 0 }}
+            >
+              {loading && <p className="text-white">Загрузка...</p>}
+              {videoSrc && !loading && (
+                <video
+                  ref={videoRef}
+                  src={videoSrc}
+                  className="w-full h-full object-cover"
+                  controls={false}
+                  loop
+                  playsInline
+                  autoPlay
+                  onClick={(e) => {
+                    const video = e.currentTarget as HTMLVideoElement;
+                    if (video.paused) {
+                      video.play();
+                    } else {
+                      video.pause();
+                    }
+                  }}
+                />
+              )}
+            </motion.div>
+          </AnimatePresence>
+          <button
+            onClick={handleFullscreen}
+            className="absolute py-2 top-2 right-2 text-black rounded p-1 z-10"
+          >
+            <FiMaximize size={24} />
+          </button>
         </div>
-        <button
-          onClick={handleFullscreen}
-          className="absolute py-2 top-2 right-2 text-black rounded p-1"
-        >
-          <FiMaximize size={24} />
-        </button>
 
         {/* Текстовые элементы */}
         {username && userId && (
@@ -232,28 +269,23 @@ const RecommendationsPage = () => {
 
         {/* Кнопки */}
         <div className="flex justify-between mt-6 w-full max-w-sm mx-auto px-4">
-          <button
+          <motion.button
             onClick={dislikeVideo}
             disabled={!videoId || loading}
             className="bg-gray-300 text-black rounded px-6 py-2"
+            whileTap={{ scale: 0.9 }}
           >
             <FiThumbsDown size={24} />
-          </button>
-          {/*<button*/}
-          {/*  disabled={true}*/}
-          {/*  className="bg-blue-500 text-white rounded px-4 py-2"*/}
-          {/*  style={{ backgroundColor: "#FEEF9E", color: "black" }}*/}
-          {/*>*/}
-          {/*  <FiRefreshCw size={24} />*/}
-          {/*</button>*/}
-          <button
+          </motion.button>
+          <motion.button
             onClick={likeVideo}
             disabled={!videoId || loading}
-            className="bg-blue-500 text-white rounded px-6 py-2"
+            className="bg-blue-500 text-black rounded px-6 py-2"
             style={{ backgroundColor: "#ABD6E0", color: "black" }}
+            whileTap={{ scale: 0.9 }}
           >
             <FiThumbsUp size={24} />
-          </button>
+          </motion.button>
         </div>
       </main>
     </div>
